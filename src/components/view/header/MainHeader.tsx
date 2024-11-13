8af3c72198f00ff4c0984b81a9c92f67a2a0c83e
김동균 <wooheuy5752@nate.com>
2024-11-13 19:57:20 +0900

    fix : MainHeader 기본 높이 설정 (#7)

--- src/components/view/header/MainHeader.tsx
+++ src/components/view/header/MainHeader.tsx
@@ -10,7 +10,7 @@ export const MainHeader = () => {
   const loginSession = getSessionStorageItem(SESSION_LOGIN_KEY)
 
   return (
-    <header className="flex-between-align relative mx-4 py-4">
+    <header className="flex-between-align relative mx-4 h-20 py-4">
       <Link to={'/home'}>
         <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center font-jalnan text-[28px] leading-9 text-blue-6">
           BROOM
