1ff9d78e757411cc0ff2de231e69aab7e61dedbe
김동균 <wooheuy5752@nate.com>
2024-11-14 00:15:54 +0900

    feat : 회원가입 api 타입 지정 (#9)

---
+++ src/types/auth.ts
@@ -0,0 +1,30 @@
+import type { IconType } from './common'
+
+export type AuthType = {
+  userId: string
+  password: string
+  nickname: string
+  dischargeYear: number
+  militaryChaplain: IconType
+}
+
+export type SignupFormType = AuthType & { confirm: string }
+
+export type LoginRequest = {
+  body: Pick<AuthType, 'userId' | 'password'>
+}
+export type LoginResponse = Pick<AuthType, 'nickname' | 'militaryChaplain'>
+
+export type SignupRequest = {
+  body: AuthType
+}
+
+export type ValidateIdRequest = {
+  body: Pick<AuthType, 'userId'>
+}
+export type ValidateIdResponse = string
+
+export type ValidateNicknameRequest = {
+  body: Pick<AuthType, 'nickname'>
+}
+export type ValidateNicknameResponse = string
