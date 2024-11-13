2402ed274999d61b466eb996563824f298830d24
김동균 <wooheuy5752@nate.com>
2024-11-14 03:09:47 +0900

    feat : 로그인 스키마 및 폼 구현 (#11)

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,8 +1,18 @@
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
-import type { SignupFormType } from '@/types'
-import { signupSchema } from '@/utils'
+import type { LoginFormType, SignupFormType } from '@/types'
+import { loginSchema, signupSchema } from '@/utils'
+
+export const useLoginForm = () => {
+  const formMethod = useForm<LoginFormType>({
+    mode: 'onSubmit',
+    reValidateMode: 'onSubmit',
+    resolver: zodResolver(loginSchema),
+  })
+
+  return formMethod
+}
 
 export const useSignupForm = () => {
   const formMethod = useForm<SignupFormType>({
