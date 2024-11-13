64cac5153948e23ee3abb510c2dd913e5205e2c9
김동균 <wooheuy5752@nate.com>
2024-11-13 16:15:36 +0900

    feat : PostBottom 구현 (#3)

---
+++ src/components/view/post/PostBottom.tsx
@@ -0,0 +1,41 @@
+import { useToggle } from '@/hooks'
+
+import { Button } from '../Button'
+import { BookmarkIcon } from '../icons/ActiveIcons'
+
+type PostBottomProps = {
+  disabled?: boolean
+  initialBookmarkState?: boolean
+  onClickBookmark: VoidFunction
+  onClickChattingButton: VoidFunction
+}
+
+export const PostBottom = ({
+  disabled = false,
+  initialBookmarkState = false,
+  onClickBookmark,
+  onClickChattingButton,
+}: PostBottomProps) => {
+  const [isBookmarked, setIsBookmarked] = useToggle(initialBookmarkState)
+
+  const handleClickBookmark = () => {
+    onClickBookmark()
+    setIsBookmarked()
+  }
+
+  return (
+    <div className="flex-align w-full gap-6 px-4 pb-[27px] pt-[9px] shadow-md">
+      <button
+        type="button"
+        className="flex-column-align shrink-0 gap-1"
+        onClick={handleClickBookmark}
+      >
+        <BookmarkIcon active={isBookmarked} />
+        <p className="p-xsmall text-grey-5">북마크</p>
+      </button>
+      <Button classname="grow" size="sm" onClick={onClickChattingButton} disabled={disabled}>
+        {disabled ? '모집 마감' : '채팅하기'}
+      </Button>
+    </div>
+  )
+}
