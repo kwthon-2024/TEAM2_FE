20d2a79596f6667818e218328115e588ccfd2a51
김동균 <wooheuy5752@nate.com>
2024-11-13 11:02:48 +0900

    chore : 메인 기본 레이아웃 설정 (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-import { useState } from 'react'
-
-import reactLogo from './assets/react.svg'
-
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="flex-center">
+      <div className="scroll relative h-svh w-full min-w-[320px] max-w-[450px] border-x">app</div>
+    </div>
   )
 }
 
