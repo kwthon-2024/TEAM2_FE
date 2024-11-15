6a34f995dd3f32a2e98d71ab8841144e714a77cc
김동균 <wooheuy5752@nate.com>
2024-11-15 12:24:44 +0900

    feat : 팀 모집 채팅 타입 설정 (#26)

--- src/types/chatting.ts
+++ src/types/chatting.ts
@@ -1,13 +1,21 @@
-type CarpoolChattingRoomType = {
+type ChattingRoomType = {
   chatRoomId: string
   authorId: string
   participantId: string
   opponentNickname: string
-  carpoolBoardTitle: string
   lastMessage: string
   lastMessageDaysAgo: number
   read: boolean
+}
+
+type CarpoolChattingRoomType = ChattingRoomType & {
+  carpoolBoardTitle: string
   carpoolBoardId: string
+}
+
+type TeammateChattingRoomType = ChattingRoomType & {
+  teamBoardTitle: string
+  teamBoardId: string
 }
 
 export type CarpoolChattingListResponse = Omit<CarpoolChattingRoomType, 'carpoolBoardId'>[]
@@ -19,4 +27,15 @@ export type CarpoolChattingIdResponse = Omit<CarpoolChattingRoomType, 'carpoolBo
 
 export type CarpoolExitChattingRoomRequest = {
   urls: Pick<CarpoolChattingRoomType, 'chatRoomId'>
+}
+
+export type TeammateChattingListResponse = Omit<TeammateChattingRoomType, 'teamBoardId'>[]
+
+export type TeammateChattingIdRequest = {
+  urls: Pick<TeammateChattingRoomType, 'teamBoardId'>
+}
+export type TeammateChattingIdResponse = Omit<TeammateChattingRoomType, 'teamBoardId'>
+
+export type TeammateExitChattingRoomRequest = {
+  urls: Pick<TeammateChattingRoomType, 'chatRoomId'>
 }
